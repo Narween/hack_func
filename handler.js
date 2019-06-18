68c353c03d51beb0b87db34864294f9062377e2a
Muriel Toulet <muriel.toulet@concur.com>
2019-06-18 12:06:47 +0200

    ...

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'This is a test for a demo...',
+      message: 'This is a test for a demo...for Steve!!!',
       input: event,
     }, null, 2),
   };
